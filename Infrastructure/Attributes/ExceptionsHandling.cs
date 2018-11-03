b954cddfa2f2637bbafda775bddc4cb1c3c43a8a
MohammadKhaled22 <Admin@DESKTOP-MND5NF8>
2018-11-03 17:49:00 +0200

    Add Exceptionss Handling Attrubute

---
+++ Infrastructure/Attributes/ExceptionsHandling.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Attributes
+{
+    public class ExceptionsHandling : Attribute, IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            var currentException = context.Exception;
+            if (currentException is OracleException)
+            {
+                // Do the Log 
+            }
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            
+        }
+    }
+}
