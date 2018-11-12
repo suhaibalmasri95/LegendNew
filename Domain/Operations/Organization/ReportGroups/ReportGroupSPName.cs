ed93435df0fad67b62a20ab0c8e1960788c80658
suhaibalmasri95 <suhaib.almasri95@hotmail.com>
2018-11-12 22:17:21 +0200

    FIXES

--- Domain/Operations/Organization/ReportGroups/ReportGroupSPName.cs
+++ Domain/Operations/Organization/ReportGroups/ReportGroupSPName.cs
@@ -8,6 +8,7 @@ namespace Domain.Operations.Organization.ReportGroups
     {
         public const string SP_INSERT_ReportGroup = "DBPK_ORG.INS_ST_REPORT_GROUP";
         public const string SP_UPADTE_ReportGroup = "DBPK_ORG.UPD_ST_REPORT_GROUP";
-        public const string SP_LOAD_ReportGroup = "DBPK_ORG.DEL_ST_REPORT_GROUP";
+        public const string SP_LOAD_ReportGroup = "DBPK_ORG.LOAD_ST_REPORT_GROUP";
+        public const string SP_DELETE_ReportGroup = "DBPK_ORG.DEL_ST_REPORT_GROUP";
     }
 }
