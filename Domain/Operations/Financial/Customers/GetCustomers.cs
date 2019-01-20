90b009a5fd44a43a31ff3db76193b7d373a92a9a
suhaibalmasri95 <suhaib.almasri95@hotmail.com>
2019-01-21 00:08:28 +0200

    fixes

--- Domain/Operations/Financial/Customers/GetCustomers.cs
+++ Domain/Operations/Financial/Customers/GetCustomers.cs
@@ -34,5 +34,26 @@ namespace Domain.Operations.Financial.Customers
 
             return await QueryExecuter.ExecuteQueryAsync<Customer>(CustomerSpName.SP_LOAD_CUSTOMER, dyParam);
         }
+        public async Task<IEnumerable> Query()
+        {
+            var dyParam = new OracleDynamicParameters();
+
+            dyParam.Add(CustomerSpParams.PARAMETER_ID, OracleDbType.Int64, ParameterDirection.Input, (object)ID ?? DBNull.Value);
+            dyParam.Add(CustomerSpParams.PARAMETER_CUSTOMER_NO, OracleDbType.Varchar2, ParameterDirection.Input, (object)CustomerNo ?? DBNull.Value);
+            dyParam.Add(CustomerSpParams.PARAMETER_NAME, OracleDbType.Varchar2, ParameterDirection.Input, (object)Name ?? DBNull.Value);
+            dyParam.Add(CustomerSpParams.PARAMETER_IND_COMP, OracleDbType.Int64, ParameterDirection.Input, (object)IndOrComp ?? DBNull.Value);
+            dyParam.Add(CustomerSpParams.PARAMETER_COMM_NAME, OracleDbType.Varchar2, ParameterDirection.Input, (object)CommName ?? DBNull.Value);
+            dyParam.Add(CustomerSpParams.PARAMETER_PHONE, OracleDbType.Varchar2, ParameterDirection.Input, (object)Phone ?? DBNull.Value);
+            dyParam.Add(CustomerSpParams.PARAMETER_MOBILE, OracleDbType.Varchar2, ParameterDirection.Input, (object)Mobile ?? DBNull.Value);
+            dyParam.Add(CustomerSpParams.PARAMETER_EMAIL, OracleDbType.Varchar2, ParameterDirection.Input, (object)Email ?? DBNull.Value);
+            dyParam.Add(CustomerSpParams.PARAMETER_ST_COM_ID, OracleDbType.Decimal, ParameterDirection.Input, (object)CompanyID ?? DBNull.Value);
+            dyParam.Add(CustomerSpParams.PARAMETER_LOC_CUST_TYPE, OracleDbType.Int64, ParameterDirection.Input, (object)CustomerType ?? DBNull.Value);
+            dyParam.Add(CustomerSpParams.PARAMETER_REF_NO, OracleDbType.Varchar2, ParameterDirection.Input, (object)ReferenceNo ?? DBNull.Value);
+            dyParam.Add(CustomerSpParams.PARAMETER_CUSTOMER_NO_OR_NAME, OracleDbType.Varchar2, ParameterDirection.Input, (object)CustomerNoOrName ?? DBNull.Value);
+            dyParam.Add(CustomerSpParams.PARAMETER_LANG_ID, OracleDbType.Decimal, ParameterDirection.Input, (object)LangID ?? DBNull.Value);
+            dyParam.Add(CustomerSpParams.PARAMETER_REF_SELECT, OracleDbType.RefCursor, ParameterDirection.Output);
+
+            return await QueryExecuter.ExecuteQueryAsync<Customer>(CustomerSpName.SP_LOAD_CUSTOMER, dyParam);
+        }
     }
 }
